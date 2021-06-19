a2a718ff0516cbaae042169a98f3ea57115ba5c7
César Maciel <jr.maciel13@gmail.com>
2021-06-19 11:46:52 -0300

    Sorteador Refratorado

--- Sorteador/app/src/main/java/com/example/sorteador/MainActivity.kt
+++ Sorteador/app/src/main/java/com/example/sorteador/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.sorteador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var texto: TextView
+    lateinit var btn : Button
+    var numeroSorteado: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super .onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        texto = findViewById(R.id.txtNumero)
+        btn = findViewById(R.id.btnSortear)
+        btn.setOnClickListener {
+            numeroSorteado = texto.text.toString().toIntOrNull()
+            SortearNumero()
+        }
     }
 
-    fun SortearNumero(view: View){
-        var texto = findViewById(R.id.txtNumero) as TextView
+
+    fun SortearNumero(){
 
         var numeroSorteado = Random().nextInt(11)
 
