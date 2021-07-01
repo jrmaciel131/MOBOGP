8de26a31a7a2745cc849b146cc725c3877d3642b
xKrypt <hkraphael79@gmail.com>
2021-07-01 19:46:32 -0300

    Listagem de Pets

---
+++ ListagemPets/app/src/main/java/com/example/listagempets/Especie.kt
@@ -0,0 +1,7 @@
+package com.example.listagempets
+
+enum class Especie (val nome: String) {
+    CACHORRO("Cachorro"),
+    GATO("Gato"),
+    MACACO("Macaco")
+}
---
+++ ListagemPets/app/src/main/java/com/example/listagempets/Idade.kt
@@ -0,0 +1,8 @@
+package com.example.listagempets
+
+enum class Idade (val idade: String) {
+    ANO(" ano"),
+    ANOS(" anos"),
+    MES(" mês"),
+    MESES(" meses")
+}
