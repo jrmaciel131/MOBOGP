a3dae02eaf2ce5c667f725ec2868705649c927f0
César Maciel <jr.maciel13@gmail.com>
2021-07-08 19:36:41 -0300

    APP12

---
+++ exFragments_APP12/app/src/main/java/com/example/exfragments_app12/Fragment02.kt
@@ -0,0 +1,21 @@
+package com.example.exfragments_app12;
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment02 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_02, container, false)
+    }
+}
---
+++ exFragments_APP12/app/src/main/java/com/example/exfragments_app12/PagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.exfragments_app12;
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(gerenciador: FragmentManager): FragmentPagerAdapter(gerenciador) {
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0)
+            Fragment01()
+        else
+            Fragment02()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return if (position == 0)
+            "Conversa"
+        else
+            "Status"
+    }
+}
