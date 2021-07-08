06eba9858774bccd05d8557b7f35364394684b0a
xKrypt <breakebite@gmail.com>
2021-07-08 19:20:34 -0300

    Cadastro de Devs

---
+++ CadastroDeDevs/app/src/main/java/com/raphael/cadastrodedevs/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.raphael.cadastrodedevs
+
+enum class Senioridade(val nome: String) {
+    JUNIOR("Junior"),
+    PLENO("Pleno"),
+    SENIOR("Senior")
+}
---
+++ CadastroDeDevs/app/src/main/java/com/raphael/cadastrodedevs/Stack.kt
@@ -0,0 +1,7 @@
+package com.raphael.cadastrodedevs
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
